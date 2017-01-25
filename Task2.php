dc625e1ae859abc40436e8dccc5f8bb9ed0205ea
Nastya <nastya.gorobets95@gmail.com>
2017-01-25 17:58:23 +0200

    Added Task2 and Task2Test

---
+++ Task2.php
@@ -0,0 +1,31 @@
+<?php
+/**
+https://www.codewars.com/kata/52de553ebb55d1fca3000371/train/php
+ * list will always be atleast 3 numbers. The missing term will never be the first or last one
+ */
+
+function findMissing($list){
+    if(count($list)<3)throw new Exception("List must be atleast 3 numbers");
+    $diff=$list[1]-$list[0];
+    $d=$diff;
+    $t=$diff;
+    $count=0;
+    for($i=1;$i<count($list)-1;$i++){
+        $t1=$list[$i];
+        $t2=$list[$i+1];
+        $d=$t2-$t1;
+        if($d==$t)$count++;
+    }
+    if($count>0){
+        $diff=$diff;
+    }else $diff=$d;
+    $res=0;
+    for($i=0;$i<count($list)-1;$i++){
+        $t1=$list[$i]+$diff;
+        $t2=$list[$i+1];
+        if($t1 != $t2){
+            $res=$t1;
+        }
+    }
+    return $res;
+}
---
+++ tests/Task2Test.php
@@ -0,0 +1,13 @@
+<?php
+require 'Task2.php';
+
+class Task2Test extends PHPUnit_Framework_TestCase
+{
+    public function testTask2(){
+        $list=[1,3,5,9,11];
+        $this->assertEquals(7,findMissing($list));
+        $list=[1,5,7];
+        $this->assertEquals(3,findMissing($list));
+    }
+
+}
