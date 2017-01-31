ee1f82cfd2ce552defc216032cbfc8e1b28468d8
Nastya <nastya.gorobets95@gmail.com>
2017-01-31 19:24:05 +0200

    Added Task10Test.php

--- tests/Task10Test.php
+++ tests/Task10Test.php
@@ -1,12 +1,14 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Nastya
- * Date: 31.01.2017
- * Time: 18:56
- */
-class Task10Test
+class Task10Test extends PHPUnit_Framework_TestCase
 {
-
+    public function testTask10(){
+        $task10=new Source\Task10();
+        $this->assertEquals("TTTT", $task10->DNA_strand("AAAA"));
+        $this->assertEquals("AAAA", $task10->DNA_strand("TTTT"));
+        $this->assertEquals("TAACG",$task10->DNA_strand("ATTGC"));
+        $this->assertEquals("ATTGC",$task10->DNA_strand("TAACG"));
+        $this->assertEquals("CATA", $task10->DNA_strand("GTAT"));
+        $this->assertEquals("GTAT", $task10->DNA_strand("CATA"));
+    }
 }
