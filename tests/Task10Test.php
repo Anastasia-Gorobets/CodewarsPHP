a5080a0f9bda7b8c154b77c2c51826845ab366c5
ReenExe <ipourri@gmail.com>
2017-01-31 22:37:03 +0200

    Использую возможности PHPUnit

--- tests/Task10Test.php
+++ tests/Task10Test.php
@@ -2,14 +2,24 @@
 
 class Task10Test extends PHPUnit_Framework_TestCase
 {
-    public function testTask10()
+    /**
+     * @dataProvider dataProvider
+     * @param $from
+     * @param $to
+     */
+    public function test($from, $to)
     {
         $task10 = new Source\Task10();
-        $this->assertEquals('TTTT', $task10->DNA_strand('AAAA'));
-        $this->assertEquals('AAAA', $task10->DNA_strand('TTTT'));
-        $this->assertEquals('TAACG', $task10->DNA_strand('ATTGC'));
-        $this->assertEquals('ATTGC', $task10->DNA_strand('TAACG'));
-        $this->assertEquals('CATA', $task10->DNA_strand('GTAT'));
-        $this->assertEquals('GTAT', $task10->DNA_strand('CATA'));
+        $this->assertEquals($from, $task10->DNA_strand($to));
+        $this->assertEquals($to, $task10->DNA_strand($from));
+    }
+
+    public function dataProvider()
+    {
+        return [
+            ['TTTT', 'AAAA'],
+            ['TAACG', 'ATTGC'],
+            ['CATA', 'GTAT'],
+        ];
     }
 }
