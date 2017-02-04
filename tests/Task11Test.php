919153b769affc8312d047f0612280523d3e249f
Nastya <nastya.gorobets95@gmail.com>
2017-02-04 19:35:37 +0200

    Changed Task11Test.php

--- tests/Task11Test.php
+++ tests/Task11Test.php
@@ -2,8 +2,11 @@
 
 class Task11Test extends PHPUnit_Framework_TestCase
 {
-    public function  testTask11(){
-
+    public function testTask11()
+    {
+        $task11 = new Source\Task11();
+        $this->assertEquals(2.5, $task11->median([1, 2, 3, 4]));
+        $this->assertEquals(3, $task11->median([3, 4, 1, 2, 5]));
+        $this->assertEquals(41, $task11->median([10, 29, 23, 94, 76, 96, 5, 85, 4, 33, 47, 41, 87]));
     }
-
 }
